1bb8f038440b0d1974b08f5ab1105b2efa9e948a
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-20 16:56:10 +0000

    update file

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import connectDB from './mongdb/connect.js';
+import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
