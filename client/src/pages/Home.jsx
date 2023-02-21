5df15935849685dc11fef34df7d0ac7897b10d63
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-21 14:56:19 +0000

    make call to get all posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,6 +17,34 @@ const Home = () => {
 
     const [searchText, setSearchText] = useState('');
 
+    useEffect(() => { 
+    const fetchPosts = async () => {
+        setLoading(true);
+
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/post', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            if(response.ok){
+
+            const result = await response.json();
+
+            setAllPosts(result.data.reverse);
+            }
+            
+        } catch (error) {
+            alert(error)
+            
+        } finally {
+            setLoading(false);
+        }
+    }
+    }, []);
+
 
   return (
     <section className="max-w-7xl mx-auto">
