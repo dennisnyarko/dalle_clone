ba9ac25da588f5af091c774dbfd2df2f70bcef7e
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-22 14:35:26 +0000

    fix error

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -24,14 +24,14 @@ router.route('/').post(async (req, res) => {
             prompt,
             n: 1,
             size: '1024x1024',
-            response_format: 'b64_json'
+            response_format: 'b64_json',
         });
 
         const image = aiResponse.data.data[0].b64_json;
         res.status(200).json({ photo: image });
     } catch (error) {
         console.log(error);
-        res.status(500).send({error: error.message})
+        res.status(500).send(error?.response.data.error.message)
 
     }
 })
