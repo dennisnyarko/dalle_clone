b35d9f2f01e2497b111b051a087bd61a364c089b
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-22 16:03:21 +0000

    update

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,7 +5,10 @@ import { downloadImage } from '../utilis'
 
 const Card = ({ _id, name, prompt, photo }) => {
   return (
-    <div>Card</div>
+    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+      <img className="w-full h-auto object-cover rounded-xl" src={photo} alt={prompt} />
+
+    </div>
   )
 }
 
